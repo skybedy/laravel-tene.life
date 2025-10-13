a6ad1e90c165aa826ed6c1b8775340e458612c4d
skybedy <skybedy@gmail.com>
2025-10-13 10:57:48 +0100

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,6 +23,7 @@
         
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+                
             </main>
         </div>
 
