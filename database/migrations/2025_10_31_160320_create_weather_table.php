e325267305010a7a2a575837b1a561c714d17ea9
skybedy <skybedy@gmail.com>
2025-10-31 17:08:59 +0000

    311025-2

---
+++ database/migrations/2025_10_31_160320_create_weather_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('measured_at')->comment('Time when the weather data was measured');
+            $table->decimal('temperature', 5, 2)->comment('Temperature in °C');
+            $table->decimal('pressure', 7, 2)->comment('Pressure in hPa');
+            $table->decimal('humidity', 5, 2)->comment('Humidity in %');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));         
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather');
+    }
+};
