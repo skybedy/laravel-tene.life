4f9cbb62f5e72603b8c1d5e778f1812e5ac2c32a
skybedy <skybedy@gmail.com>
2025-11-03 16:31:46 +0000

    rychly fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-dam to tam 
         <title>Tenerife | Los Cristianos | Webcam |</title>
 
         <!-- Fonts -->
