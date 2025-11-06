18d2380ef6300ebc8a22b61d669cfeb2d0ba00d4
skybedy <skybedy@gmail.com>
2025-11-06 16:45:56 +0000

    Zmena ctyrciferneho letopoctu na dvouciferny

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
    @php
         $timestamp = filemtime('images/tenelife.jpg');
         $hour = date('H', $timestamp);
-        $date = date('j. n. Y', $timestamp);
+        $date = date('j. m. y', $timestamp);
         $time = date("$hour:i", $timestamp);
         $currentLocale = app()->getLocale();
    @endphp
